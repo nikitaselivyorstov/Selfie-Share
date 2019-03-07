2b23961bb641c54f19034c1ba79ec9ed94da187e
Nikita <nikitaseliverstov@gmail.com>
2019-03-07 23:31:40 +0300

    Initial Commit

---
+++ Selfie Share/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Selfie Share
+//
+//  Created by Nikita Seliverstov  on 07/03/2019.
+//  Copyright © 2019 Nikita Seliverstov . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
